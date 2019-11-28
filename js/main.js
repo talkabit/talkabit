74b74c94e603fb2793499f740af24071004dd9fb
David Reis <david.alex.reis.97@gmail.com>
2019-11-28 17:40:34 +0000

    Fixed pwa aspects

--- js/main.js
+++ js/main.js
@@ -1,27 +1,10 @@
-
-
 window.onload = () => {
     'use strict';
-
-    let notifStatus = document.getElementById("notifStatus");
-
-    Notification.requestPermission(status => {
-        notifStatus.innerHTML = "Notification Status: " + status;
-    })
+    Notification.requestPermission(status => {});
 
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js');
+        navigator.serviceWorker.register('/sw.js');
     }else{
-        console.log("Could not register service worker")
+        console.error("Could not register service worker")
     }
   }
-
-function showNotif(){
-    navigator.serviceWorker.getRegistration().then(
-        reg => {
-            reg.showNotification("Talk A Bit '20",{
-                body:"Bem vindo ao melhor TaB de sempre!"
-            });
-        }
-    )
-}
--- sw.js
+++ sw.js
@@ -1,25 +1,116 @@
-var cacheName = 'hello-pwa';
+var cacheName = 'talkabit-pwa';
 var filesToCache = [
-  '/',
+  '/favicon.ico',
   '/index.html',
+  '/css/bootstrap/css/bootstrap.min.css',
+  '/css/bootstrap/css/bootstrap-reboot.min.css.map',
+  '/css/bootstrap/css/bootstrap.css',
+  '/css/bootstrap/css/bootstrap-grid.css.map',
+  '/css/bootstrap/css/bootstrap-grid.min.css',
+  '/css/bootstrap/css/bootstrap.css.map',
+  '/css/bootstrap/css/bootstrap.min.css.map',
+  '/css/bootstrap/css/bootstrap-reboot.min.css',
+  '/css/bootstrap/css/bootstrap-reboot.css',
+  '/css/bootstrap/css/bootstrap-grid.css',
+  '/css/bootstrap/css/bootstrap-grid.min.css.map',
+  '/css/bootstrap/css/bootstrap-reboot.css.map',
+  '/css/bootstrap/js/bootstrap.bundle.js',
+  '/css/bootstrap/js/bootstrap.bundle.min.js.map',
+  '/css/bootstrap/js/bootstrap.bundle.js.map',
+  '/css/bootstrap/js/bootstrap.js',
+  '/css/bootstrap/js/bootstrap.bundle.min.js',
+  '/css/bootstrap/js/popper.min.js.map',
+  '/css/bootstrap/js/bootstrap.min.js',
+  '/css/bootstrap/js/popper.min.js',
+  '/css/bootstrap/js/bootstrap.js.map',
+  '/css/bootstrap/js/bootstrap.min.js.map',
+  '/css/speakers.css',
+  '/css/fontawesome/css/all.css',
+  '/css/fontawesome/css/v4-shims.css',
+  '/css/fontawesome/css/brands.min.css',
+  '/css/fontawesome/css/solid.css',
+  '/css/fontawesome/css/brands.css',
+  '/css/fontawesome/css/regular.css',
+  '/css/fontawesome/css/all.min.css',
+  '/css/fontawesome/css/svg-with-js.min.css',
+  '/css/fontawesome/css/v4-shims.min.css',
+  '/css/fontawesome/css/fontawesome.min.css',
+  '/css/fontawesome/css/svg-with-js.css',
+  '/css/fontawesome/css/regular.min.css',
+  '/css/fontawesome/css/fontawesome.css',
+  '/css/fontawesome/css/solid.min.css',
+  '/css/fontawesome/webfonts/fa-solid-900.ttf',
+  '/css/fontawesome/webfonts/fa-regular-400.svg',
+  '/css/fontawesome/webfonts/fa-regular-400.woff2',
+  '/css/fontawesome/webfonts/fa-solid-900.eot',
+  '/css/fontawesome/webfonts/fa-brands-400.svg',
+  '/css/fontawesome/webfonts/fa-regular-400.woff',
+  '/css/fontawesome/webfonts/fa-brands-400.eot',
+  '/css/fontawesome/webfonts/fa-solid-900.svg',
+  '/css/fontawesome/webfonts/fa-solid-900.woff',
+  '/css/fontawesome/webfonts/fa-regular-400.ttf',
+  '/css/fontawesome/webfonts/fa-solid-900.woff2',
+  '/css/fontawesome/webfonts/fa-brands-400.woff2',
+  '/css/fontawesome/webfonts/fa-brands-400.woff',
+  '/css/fontawesome/webfonts/fa-brands-400.ttf',
+  '/css/fontawesome/webfonts/fa-regular-400.eot',
   '/css/style.css',
-  '/js/main.js'
+  '/css/fonts/Roboto-Medium.ttf',
+  '/css/fonts/Roboto-Light.ttf',
+  '/css/fonts/decima-light-webfont.woff',
+  '/css/fonts/Decima-Pro-A.ttf',
+  '/css/fonts/Roboto-Regular.ttf',
+  '/css/fonts/Decima-Pro.ttf',
+  '/css/fonts/Roboto-MediumItalic.ttf',
+  '/css/fonts/Karben_105_W00_Bold.ttf',
+  '/css/fonts/Decima-Pro-A-Bold.ttf',
+  '/css/fonts/Roboto-ThinItalic.ttf',
+  '/css/fonts/Roboto-BoldItalic.ttf',
+  '/css/fonts/Roboto-LightItalic.ttf',
+  '/css/fonts/Decima-Pro-A-Light.ttf',
+  '/css/fonts/Roboto-Italic.ttf',
+  '/css/fonts/Roboto-BlackItalic.ttf',
+  '/css/fonts/Roboto-Bold.ttf',
+  '/css/fonts/Decima-Pro-Bold.ttf',
+  '/css/fonts/Roboto-Thin.ttf',
+  '/css/fonts/Roboto-Black.ttf',
+  '/css/fonts/Calibri.ttf',
+  '/css/footer.css',
+  '/images/logo_filled.png',
+  '/images/feup.jpeg',
+  '/images/logo_sem.svg',
+  '/images/placeholder_member.png',
+  '/images/logo_sem.png',
+  '/images/musky.jpg',
+  '/images/logo_lg.svg',
+  '/images/logo_com.png',
+  '/images/logo.png',
+  '/images/logo_com.svg',
+  '/images/logo_lg.png',
+  '/js/organization.js',
+  '/js/hide-navbar.js',
+  '/js/main.js',
+  '/manifest.json',
+  '/pages/speakers.html',
+  '/sw.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
+      return cache.addAll(filesToCache).catch((error) => {
+        console.log(error);
+      });
     })
   );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+//Network then cache approach
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
     })
   );
 });
