993260b14e30724d11879695576425ff284fb7d9
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2020-10-17 18:43:45 +0100

    Set metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Talk A Bit`,
+    description: `Talk A Bit 2021 Website`,
+    author: `@talkabit`,
   },
   plugins: [
     `gatsby-plugin-sharp`,
