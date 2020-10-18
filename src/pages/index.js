0aa0df227ba8a25bd17d3f7c55dca7fa7d4f3efa
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2020-10-18 11:53:46 +0100

    Replace prettier with eslint, remove husky, add CI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,38 @@
-import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import Seo from "../components/seo";
 
 const IndexPage = () => {
-  const message = useStaticQuery(graphql`
+    const message = useStaticQuery(graphql`
     query MyQuery {
       dataJson {
         message
       }
     }
-  `)
+  `);
 
-  console.log(message)
+    return (
+        <Layout>
+            <Seo title="Home" />
+            <h1>Hi people</h1>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
+            <p>
+                <em>
+                    {message.dataJson.message}
+                </em>
+            </p>
+            <div style={{ maxWidth: "300px", marginBottom: "1.45rem" }}>
+                <Image />
+            </div>
+            <Link to="/page-2/">Go to page 2</Link>
+            {" "}
+            <br />
+        </Layout>
+    );
+};
 
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <p>
-        <em>{message.dataJson.message}</em>
-      </p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </Layout>
-  )
-}
-
-export default IndexPage
+export default IndexPage;
