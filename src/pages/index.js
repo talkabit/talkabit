721b23660817042adf7f4ec5dc455a552035a059
Pedro Neto <pedroneto@Pedros-MacBook-Pro.local>
2020-11-16 22:39:28 +0000

    calendar multiple options

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,40 +6,85 @@ import Seo from "../components/seo";
 
 import img from "../images/coming-soon.png";
 
+import AddToCalendarHOC from 'react-add-to-calendar-hoc';
+import PropTypes from "prop-types";
 
-const ComingSoon = () => (
-    <div className={styles.container}>
-        <Seo title="Coming Soon" />
-        <div className={styles.text_container}>
-            <h2 className={styles.event}>Talk a Bit 2021</h2>
-            <div className={styles.dates}>
-                <h3 className={styles.text}>FEB 12 & 13</h3>
-                <a
-                    className={styles.link}
-                    rel="noreferrer"
-                    target="_blank"
-                    href="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20210212T000000Z/20210213T000000Z&location=Online&text=Talk+a+Bit+2021&details"
-                >
-                    <p className={styles.button}>
-                        Add to calendar
+const ComingSoon = () => {
+
+    const event = {
+        description: '',
+        duration: '',
+        endDatetime: '20210213T000000Z',
+        location: 'Online',
+        startDatetime: '20210212T000000Z',
+        title: 'Talk a Bit 2021',
+    }
+
+    const Button = ({ children, onClick }) => (
+        <button onClick={onClick}>
+            {children}
+        </button>
+    );
+
+    Button.propTypes = {
+        children: PropTypes.node.isRequired,
+        onClick: PropTypes.func.isRequired,
+    };
+
+    const Modal = ({ children }) => (
+        <div className={styles.link_container}>
+            {children}
+        </div>
+
+    );
+
+    Modal.propTypes = {
+        children: PropTypes.node.isRequired,
+    };
+
+    const AddToCalendarModal = AddToCalendarHOC(Button, Modal);
+
+    return (
+        <div className={styles.container}>
+            <Seo title="Coming Soon" />
+            <div className={styles.text_container}>
+                <h2 className={styles.event}>Talk a Bit 2021</h2>
+                <div className={styles.dates}>
+                    <h3 className={styles.text}>FEB 12 & 13</h3>
+                    {/* <a
+                        className={styles.link}
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20210212T000000Z/20210213T000000Z&location=Online&text=Talk+a+Bit+2021&details"
+                    >
+                        <p className={styles.button}>
+                            Add to calendar
                     </p>
-                </a>
-            </div>
-            <h2 className={styles.slogan}>Let&#39;s talk a bit about technology.</h2>
-            <h4 className={styles.description}>
-                A student-organized interactive online experience, free for everyone.&nbsp;
+                    </a> */}
+                    <AddToCalendarModal
+                        className={styles.button}
+                        linkProps={{
+                            className: styles.link,
+                        }}
+                        event={event}
+                    />
+                </div>
+                <h2 className={styles.slogan}>Let&#39;s talk a bit about technology.</h2>
+                <h4 className={styles.description}>
+                    A student-organized interactive online experience, free for everyone.&nbsp;
                 <strong>More details soon.</strong>
-            </h4>
-            <p className={styles.contact}>
-                Contact us at&nbsp;
+                </h4>
+                <p className={styles.contact}>
+                    Contact us at&nbsp;
                 <a className={styles.email} href="mailto:geral@talkabit.org">geral@talkabit.org</a>
-            </p>
-            <a className={styles.editions} href="https://talkabit.org/2020/">Check out last year&#39;s edition here.</a>
+                </p>
+                <a className={styles.editions} href="https://talkabit.org/2020/">Check out last year&#39;s edition here.</a>
+            </div>
+            <div className={styles.img_container}>
+                <img className={styles.img} src={img} />
+            </div>
         </div>
-        <div className={styles.img_container}>
-            <img className={styles.img} src={img} />
-        </div>
-    </div>
-);
+    );
+}
 
 export default ComingSoon;
