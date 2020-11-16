bd780b9e457d5fe71040a5d2a3d88b788077440e
Pedro Neto <pedroneto@Pedros-MacBook-Pro.local>
2020-11-16 14:08:24 +0000

    form cleaning

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -14,12 +14,11 @@ const Newsletter = () =>
             <input type="hidden" name="zf_redirect_url" value="" />
             <input type="hidden" name="zc_gad" value="" />
             <h2>Newsletter Sign Up</h2>
-            <p />
             <label>
                 Email
                 <em>*</em>
             </label>
-            <input type="text" maxLength="255" name="Email" />
+            <input type="email" maxLength="255" name="Email" />
             <button type="submit">
                 <em>Submit</em>
             </button>
