5942ae0b844f4ed234540570ac3609fea5958443
Pedro Neto <pedroneto@Pedros-MacBook-Pro.local>
2021-01-25 08:45:22 +0100

    sponsors component simple

--- src/components/sponsor/sponsor.js
+++ src/components/sponsor/sponsor.js
@@ -2,35 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import Img from "gatsby-image";
 
-import styles from "../../styles/sponsors.module.css"
+import styles from "../../styles/sponsors.module.css";
 
-const Sponsor = ({ name, tier, img }) => {
+const Sponsor = ({ tier, img }) => {
 
     function tierToClass(tier) {
-		let tierClass;
-		if (tier === "0-Gold") {
-			tierClass = styles.gold;
-		}
-		else if (tier === "1-Silver") {
-			tierClass = styles.silver;
-		} else
-			tierClass = styles.bronze;
+        let tierClass;
+        if (tier === "0-Gold") {
+            tierClass = styles.gold;
+        } else if (tier === "1-Silver") {
+            tierClass = styles.silver;
+        } else
+            tierClass = styles.bronze;
 
-			return tierClass;
+        return tierClass;
     }
-    
+
     return (
         <div className={tierToClass(tier)}>
             <Img fluid={img.childImageSharp.fluid} className={styles.logo} />
-            {/* <p style={{ textAlign: "center" }}>
-                {tier}
-            </p> */}
         </div>
     );
-}
+};
 
 Sponsor.propTypes = {
-    name: PropTypes.string.isRequired,
+    tier: PropTypes.string.isRequired,
     img: PropTypes.shape({
         childImageSharp: PropTypes.shape({
             fluid: PropTypes.object,
