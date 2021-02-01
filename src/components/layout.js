6f1a1d7d842f92998008f9eb5677ed43530f75da
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2021-02-01 10:47:56 +0000

    Adjust sponsor images

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,8 +11,6 @@ import { useStaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
 import Social from "./social";
-import Sponsors from "./sponsor/sponsors";
-
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "../styles/layout.module.css";
@@ -46,7 +44,6 @@ const Layout = ({ children, title }) => {
                     {children}
                 </main>
             </Container>
-            {/* <Sponsors /> */}
             <div className={styles.footer}>
                 <div className={styles.footerContainer}>
                     <Social />
