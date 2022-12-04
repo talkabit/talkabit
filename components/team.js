9c606d7db25fd3526c4a68f95c7c3742a4aec45c
Pedro <up201809694@fe.up.pt>
2022-12-04 02:33:57 +0000

    Change photo extensions

--- components/team.js
+++ components/team.js
@@ -11,7 +11,7 @@ export default function team() {
                 <div className="chairman_container">
                     <img
                         className="member_image"
-                        src="/assets/team/Samuel_Fernandes.jpeg"
+                        src="/assets/team/Samuel_Fernandes.JPEG"
                     />
                     <p className="member_name">Samuel Fernandes</p>
                 </div>
@@ -23,14 +23,14 @@ export default function team() {
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Juan_Illescas.jpeg"
+                                src="/assets/team/Juan_Illescas.JPEG"
                             />
                             <p className="member_name">Juan Illescas</p>
                         </li>
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Pedro_Ponte.jpeg"
+                                src="/assets/team/Pedro_Ponte.JPEG"
                             />
                             <p className="member_name">Pedro Ponte</p>
                         </li>
@@ -44,21 +44,21 @@ export default function team() {
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Beatriz_Mendes.jpeg"
+                                src="/assets/team/Beatriz_Mendes.JPEG"
                             />
                             <p className="member_name">Beatriz Mendes</p>
                         </li>
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Henrique_Pereira.jpeg"
+                                src="/assets/team/Henrique_Pereira.JPEG"
                             />
                             <p className="member_name">Henrique Pereira</p>
                         </li>
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Pedro_Queiros.jpeg"
+                                src="/assets/team/Pedro_Queiros.JPEG"
                             />
                             <p className="member_name">Pedro Queirós</p>
                         </li>
@@ -72,14 +72,14 @@ export default function team() {
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Diana_Freitas.jpeg"
+                                src="/assets/team/Diana_Freitas.JPEG"
                             />
                             <p className="member_name">Diana Freitas</p>
                         </li>
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Mariana_Ramos.jpeg"
+                                src="/assets/team/Mariana_Ramos.JPEG"
                             />
                             <p className="member_name">Mariana Ramos</p>
                         </li>
@@ -93,21 +93,21 @@ export default function team() {
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Luis_Costa.jpeg"
+                                src="/assets/team/Luis_Costa.JPEG"
                             />
                             <p className="member_name">Luís Costa</p>
                         </li>
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Paulo_Ribeiro.jpeg"
+                                src="/assets/team/Paulo_Ribeiro.JPEG"
                             />
                             <p className="member_name">Paulo Ribeiro</p>
                         </li>
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Pedro_Azevedo.jpeg"
+                                src="/assets/team/Pedro_Azevedo.JPEG"
                             />
                             <p className="member_name">Pedro Azevedo</p>
                         </li>
@@ -121,14 +121,14 @@ export default function team() {
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Daniel_Monteiro.jpeg"
+                                src="/assets/team/Daniel_Monteiro.JPEG"
                             />
                             <p className="member_name">Daniel Monteiro</p>
                         </li>
                         <li className="member">
                             <img
                                 className="member_image"
-                                src="/assets/team/Rita_Mota.jpeg"
+                                src="/assets/team/Rita_Mota.JPEG"
                             />
                             <p className="member_name">Rita Mota</p>
                         </li>
