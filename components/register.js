21c6d570da8444dedbdcedc98c0e555cd7fe142f
Pedro <up201809694@fe.up.pt>
2023-01-04 17:48:54 +0000

    Add link to register

--- components/register.js
+++ components/register.js
@@ -6,7 +6,11 @@ export default function Register() {
         <div className={style.background}>
             <div className={style.register}>
                 <p className={style.p0}>Did we convince you already?</p>
-                <a href="#" target="_blank" rel="noopener noreferrer">
+                <a
+                    href="https://hello.last2ticket.com/event/4844"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <button className="register-btn">Register Now</button>
                 </a>
             </div>
